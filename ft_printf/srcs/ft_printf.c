83657063a4d6be459c228078625540ea013634de
Alex Pellier <apellier@shi-r3-p1.s19.be>
2023-04-07 17:21:33 +0200

    add comment

--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -14,35 +14,33 @@ int	ft_printf(const char *input, ...)
 			if (input[i + 1] == 'c')
 				ft_putchar_fd(va_arg(arglist, char), 1);
 			if (input[i + 1] == 's')
-				ft_putstr_fd(va_arg(arglist, char), 1);
+				ft_putstr_fd(va_arg(arglist, char *), 1);
 			if (input[i + 1] == 'p')
-				ft_putstr_fd(va_arg(arglist, char), 1);
+				ft_putstr_fd(va_arg(arglist, char), 1); // the void * ponter argument has to be printed in hexadecimal format
 			if (input[i + 1] == 'd')
-				ft_putnbr_fd(va_arg(arglist, char), 1);
+				ft_putnbr_fd(va_arg(arglist, int), 1);
 			if (input[i + 1] == 'i')
-				ft_putstr_fd(va_arg(arglist, char), 1);
+				ft_putstr_fd(va_arg(arglist, char), 1); // prints an integer in base 10
 			if (input[i + 1] == 'u')
-				ft_putstr_fd(va_arg(arglist, char), 1);
+				ft_putstr_fd(va_arg(arglist, char), 1); // prints an unsigned decimal (base 10) number
 			if (input[i + 1] == 'x')
-				ft_putstr_fd(va_arg(arglist, char), 1);
+				ft_putstr_fd(va_arg(arglist, char), 1); // prints a number in hexadecimal (base 16) lowercase format
 			if (input[i + 1] == 'X')
-				ft_putstr_fd(va_arg(arglist, char), 1);
+				ft_putstr_fd(va_arg(arglist, char), 1); // prints a number in hexadecimal (base 16) lowercase format
 			if (input[i + 1] == '%')
-				ft_putstr_fd(va_arg(arglist, char), 1);
+				ft_putchar_fd('%', 1);
 			else
 				return (-1);
 		}
 		i++;
 	}
-
 	va_end(arglist);
-
 }
 
-/* #include <stdio.h>
+#include <stdio.h>
 
 int	main()
 {
 	int a = 42;
-	printf("%s", a);
-} */
+	printf("%%");
+}
